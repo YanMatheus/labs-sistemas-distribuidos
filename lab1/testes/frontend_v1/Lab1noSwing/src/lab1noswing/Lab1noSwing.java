21736e5cd52d15f556db3b77a9b32ed211f5f23a
Yan Matheus <ymgf@icomp.ufam.edu.br>
2018-04-14 14:20:32 -0400

    add JDialog abrindo no início

--- lab1/testes/frontend_v1/Lab1noSwing/src/lab1noswing/Lab1noSwing.java
+++ lab1/testes/frontend_v1/Lab1noSwing/src/lab1noswing/Lab1noSwing.java
@@ -1,29 +1,40 @@
 package lab1noswing;
 import javax.swing.*;
-import lab1noswing.ConnectionInputDialog;
+import lab1noswing.MainWindow;
+import lab1noswing.ConnectionDialog;
 /**
  *
  * @author Yan
  */
 public class Lab1noSwing {
-    static JFrame frame = new JFrame();
-    static String userIP, userPort; 
+    static JPanel frame = new JPanel();
+    static JFrame jframe = new JFrame();
+    static JDialog connectionInput = new ConnectionDialog(jframe,true);
+    public String userIP, userPort; 
     
    
+    
+    public void setConnections(String IP, String Port){//método usado em ConnectionDialog para pegar os dados de entrada e passar para cá como string;
+        userIP = IP;
+        userPort = Port;
+    }
+    
+    public String getUserIP(){
+        return userIP;
+    }
+     public String getUserPort(){
+        return userPort;
+    }
+    
     public static void main(String[] args) {
+      
+        MainWindow mainWindow = new MainWindow();
+        connectionInput.setLocationRelativeTo(null);
         
         
-        ConnectionInputDialog dialog = new ConnectionInputDialog(frame, true);
-        
-        
-        if(dialog.isReady == true){
-            
-                userIP = dialog.getIPInput();
-                userPort = dialog.getPortInput();
-                System.out.println("IP inserido: "+userIP+ "/ Porta inserida: "+userPort);
-                //Começa conexão
-        }
-        dialog.setVisible(true);
+        mainWindow.setLocationRelativeTo(null);
+        mainWindow.setVisible(true);
+      
     }
     
 }
