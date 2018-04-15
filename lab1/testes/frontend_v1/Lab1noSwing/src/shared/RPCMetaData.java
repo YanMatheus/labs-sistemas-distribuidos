1202632f259f69b57421f5840d5b09d915b9fb01
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2018-04-15 19:38:59 -0400

    add representação dos meta-dados da RPC

---
+++ lab1/testes/frontend_v1/Lab1noSwing/src/shared/RPCMetaData.java
@@ -0,0 +1,68 @@
+package shared;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Estrutura para representar os dados que indicarão
+ * a função remota e, ao mesmo tempo, os seus valores
+ * de entrada (parâmetros). Assim, espera-se evitar
+ * o envio de múltiplas mensagens do client onde cada
+ * um seria um argumento. De forma que, agora, o server
+ * verificará apenas se a RP (Remote Procedure) com
+ * o `id` (máximo 0x7FFF) passado existe. O servidor
+ * apenas lerá um objeto desta classe, enquanto o
+ * processo cliente irá construí-lo.
+ *
+ * @author micael
+ */
+public class RPCMetaData implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // Identificadores dos procedimentos remotos disponíveis:
+    public static final short ID_RP_DESVIOPADRAO = 0x1;
+    public static final short ID_RP_MYCOPY= 0x2;
+
+    // Identificador do procedimento remoto requisitado
+    private short id;
+
+    // Lista de argumentos passados pelo processo cliente
+    private ArrayList<Object> args;
+
+    /**
+     *
+     * @param funcId - O identificador único da remote procedure.
+     * @param funcParams - Os parâmeros para
+     */
+    public RPCMetaData(short funcId, ArrayList<Object> funcParams) {
+        this.id = funcId;
+        this.args = funcParams;
+    }
+
+    /**
+     *
+     * @param funcId - O identificador único da remote procedure.
+     */
+    public RPCMetaData(short funcId) {
+        this(funcId, null);
+    }
+
+    /**
+     * Para obter o ID da RP,
+     * que será útil para o server identificar a sua procedure.
+     * @return O identificador único da remote procedure.
+     */
+    public short getId() {
+        return this.id;
+    }
+
+    /**
+     * Para obter os argumentos da RP,
+     * que será útil para o server passar os parâmetros
+     * de entrada da sua procedure.
+     * @return A lista de argumentos para a remote procedure.
+     */
+    public ArrayList<Object> getArgs() {
+        return this.args;
+    }
+}
