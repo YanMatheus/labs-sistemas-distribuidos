fd7d4759520c866766e026fe989df95ce632f875
Micael Levi <mllc@icomp.ufam.edu.br>
2018-04-14 20:25:51 -0400

    interfaces movidas

--- lab1/testes/exemplo_rpc_java/src/server/ConnectionProtocol.java
+++ lab1/testes/exemplo_rpc_java/src/server/ConnectionProtocol.java
@@ -7,10 +7,6 @@ import java.net.*;
 import java.util.logging.*;
 import java.util.ArrayList;
 
-@FunctionalInterface
-interface RunnableRemoteProcedure {
-  void run(RPCMetaData rmd, ObjectOutputStream out) throws EOFException, IOException;
-}
 
 /**
  *
@@ -20,51 +16,6 @@ public class ConnectionProtocol {
   private Socket socket;
   ObjectInputStream in;
   ObjectOutputStream out;
-
-  /**
-   * Realiza as ações para o
-   * procedimento remoto `Somar`.
-   * @throws EOFException - if this input stream reaches the end before reading four bytes.
-   * @throws IOException - the stream has been closed and the contained input stream does not support reading after close, or another I/O error occurs.
-   */
-  static void doSomar(RPCMetaData rmd, ObjectOutputStream out) throws EOFException, IOException {
-    ArrayList<Object> args = rmd.getArgs();
-
-    int arg1 = (int) args.get(0);
-    int arg2 = (int) args.get(1);
-    InfoLog.printToStdout("client called RPC somar(%d, %d)", arg1, arg2);
-
-    // realizar a operação do procedimento `somar`
-    int result = arg1 + arg2;
-
-    // [send] escrever o resultado do procedimento `somar`
-    out.writeInt(result);
-    out.flush();
-    InfoLog.printToStdout("sended %s '%d' to client", "int", result);
-  }
-
-  /**
-   * Realiza as ações para o
-   * procedimento remoto `Multiplicar`.
-   * @throws EOFException - if this input stream reaches the end before reading four bytes.
-   * @throws IOException - the stream has been closed and the contained input stream does not support reading after close, or another I/O error occurs.
-   */
-  static void doMultiplicar(RPCMetaData rmd, ObjectOutputStream out) throws EOFException, IOException {
-    ArrayList<Object> args = rmd.getArgs();
-
-    int arg1 = (int) args.get(0);
-    int arg2 = (int) args.get(1);
-    InfoLog.printToStdout("client called RPC multiplicar(%d, %d)", arg1, arg2);
-
-    // realizar a operação do procedimento `multiplicar`
-    int result = arg1 * arg2;
-
-    // [send] escrever o resultado do procedimento `multiplicar`
-    out.writeInt(result);
-    out.flush();
-    InfoLog.printToStdout("sended %s '%d' to client", "int", result);
-  }
-
 
   /**
    * Inicializa uma conexão com um um socket
