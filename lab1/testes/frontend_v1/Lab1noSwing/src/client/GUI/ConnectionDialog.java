ec4fd023fbdda8e149b7bc17049af6ec0e55de0b
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2018-04-15 21:26:48 -0400

    indentação corrigida

--- lab1/testes/frontend_v1/Lab1noSwing/src/client/GUI/ConnectionDialog.java
+++ lab1/testes/frontend_v1/Lab1noSwing/src/client/GUI/ConnectionDialog.java
@@ -176,8 +176,8 @@ public class ConnectionDialog extends javax.swing.JDialog {
 
     private void portTextFieldKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_portTextFieldKeyTyped
         char enter = evt.getKeyChar();
-        if (!(Character.isDigit(enter))
-            || portTextField.getText().trim().length() >= 5) evt.consume();
+        if ( !Character.isDigit(enter)
+          || portTextField.getText().trim().length() >= 5) evt.consume();
     }//GEN-LAST:event_portTextFieldKeyTyped
 
     private void ipTextFieldKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_ipTextFieldKeyReleased
